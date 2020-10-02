1b7e91749e816b99a5139645eb594567679a706d
Sigi Jaeckel <sigij55@gmail.com>
2020-10-02 12:28:33 -0400

    completed ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,28 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    # dic = {}
+    # lists = dict((l) for l in arrays)
+
+    # dic = {}
+    # for i in range(len(arrays)):
+    #     dic[i] = dict.fromkeys(arrays[i], 0)
+
+    dic = []
+    for array in arrays:
+        dic.append(dict.fromkeys(array, 0))
+    
+    result = dic.pop()
+    for d in dic:
+        result = result & d.keys()
+
+    # result = list(reduce(lambda x, y: x & y.keys(), dic))
+
+    # result = dic[0]
+    # for i in range(1, len(dic)-1):
+    #     result = result & dic[i].keys()
+
+    result = list(result)
 
     return result
 
